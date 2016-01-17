f2eaadd26c847bb27754ae9d5a79720fb1358d76
Tim Jarratt <tjarratt@gmail.com>
2016-01-17 10:00:30 -0800

    Add test that demonstrates the hang
    To see this yourself, run this command
    
    xcodebuild test -scheme DemonstrateXcodeHang -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.1'
    
    (substituting the appropriate simulator if 9.1 is not available)

--- DemonstrateXcodeHangTests/DemonstrateXcodeHangTests.m
+++ DemonstrateXcodeHangTests/DemonstrateXcodeHangTests.m
@@ -1,11 +1,3 @@
-//
-//  DemonstrateXcodeHangTests.m
-//  DemonstrateXcodeHangTests
-//
-//  Created by Tim Jarratt on 1/17/16.
-//  Copyright © 2016 General Linear Group. All rights reserved.
-//
-
 #import <XCTest/XCTest.h>
 
 @interface DemonstrateXcodeHangTests : XCTestCase
@@ -14,26 +6,9 @@
 
 @implementation DemonstrateXcodeHangTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testEXCBADACCESS {
+    void (^whoops)() = nil;
+    whoops();
 }
 
 @end
